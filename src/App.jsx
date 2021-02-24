eca13e3a25ef8f6cc543b6625f1c1f74e3613529
Mike Morris <eight20eight@gmail.com>
2021-02-24 17:45:42 -0500

    brought in APi and tested

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,16 @@
-import React from 'react'
+import React, { useEffect, useState } from 'react'
+import axios from 'axios'
 
 export function App() {
-  return <div>Hello, World!</div>
+  const [currencyResults, setCurrencyResults] = useState({})
+  const [amount, setsAmount] = useState(1)
+
+  useEffect(async () => {
+    const resp = await axios.get(
+      'https://api.ratesapi.io/api/2010-01-12?base=USD '
+    )
+    setCurrencyResults(resp)
+  }, [])
+
+  return <></>
 }
