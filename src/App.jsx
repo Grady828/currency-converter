5e0aefda50e7bdc38bce78758ca29edd45e69e1b
Mike Morris <eight20eight@gmail.com>
2021-02-24 19:46:07 -0500

    added input number and returning currency rates

--- src/App.jsx
+++ src/App.jsx
@@ -2,15 +2,36 @@ import React, { useEffect, useState } from 'react'
 import axios from 'axios'
 
 export function App() {
-  const [currencyResults, setCurrencyResults] = useState({})
-  const [amount, setsAmount] = useState(1)
+  const [currencyResults, setCurrencyResults] = useState({ rates: [] })
+  const [amount, setAmount] = useState(1)
 
+  // @ts-ignore
   useEffect(async () => {
     const resp = await axios.get(
-      'https://api.ratesapi.io/api/2010-01-12?base=USD '
+      'https://api.ratesapi.io/api/latest?base=USD  '
     )
-    setCurrencyResults(resp)
+    setCurrencyResults(resp.data)
   }, [])
 
-  return <></>
+  return (
+    <>
+      <ul>
+        {Object.entries(
+          // @ts-ignore
+          currencyResults.rates
+        ).map(([currencyCode, currencyDetails]) => {
+          return (
+            <li key={currencyCode}>
+              {currencyCode} : {currencyDetails}
+            </li>
+          )
+        })}
+      </ul>
+      <input
+        type="number"
+        onChange={(event) => setAmount(event.target.value)}
+        value={amount}
+      />
+    </>
+  )
 }
